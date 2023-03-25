b1efcfdb159abb4f910243b8e46bfd5d11fb6168
Dmytro Iovenko <iovenko@gmail.com>
2023-03-24 22:42:05 -0400

    change filter processes url

--- review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
+++ review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
@@ -16,6 +16,8 @@ public class SecurityConfiguration {
 	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setFilterProcessesUrl("/api/auth");
         http        
             .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
             .and().csrf().disable()
@@ -23,7 +25,7 @@ public class SecurityConfiguration {
             .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
             .anyRequest().authenticated()
             .and()
-            .addFilter(new AuthenticationFilter())
+            .addFilter(authenticationFilter)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
