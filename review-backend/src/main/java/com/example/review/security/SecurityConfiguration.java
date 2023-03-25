0abe0d945b12b41610761be37c0d180467e10231
Dmytro Iovenko <iovenko@gmail.com>
2023-03-24 22:36:23 -0400

    add custom authentication filter

--- review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
+++ review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
@@ -3,7 +3,10 @@ package com.example.review.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.review.security.filter.AuthenticationFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -13,11 +16,15 @@ public class SecurityConfiguration {
 	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http        
             .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
             .and().csrf().disable()
-            .httpBasic() // use Basic Authentication
-            ;
+            .authorizeRequests()
+            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new AuthenticationFilter())
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
