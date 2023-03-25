eb3a9d2e8073514775adbc4962092f82468df5dd
Dmytro Iovenko <iovenko@gmail.com>
2023-03-24 22:59:04 -0400

    add exception handler for filter

--- review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
+++ review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.review.security.filter.AuthenticationFilter;
+import com.example.review.security.filter.ExceptionHandlerFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -25,6 +26,7 @@ public class SecurityConfiguration {
             .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
             .anyRequest().authenticated()
             .and()
+            .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
             .addFilter(authenticationFilter)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
