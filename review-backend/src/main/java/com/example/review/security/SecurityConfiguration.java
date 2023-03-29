edc094b0aef2155cdd91e942da1a5e3959cab3cb
Dmytro Iovenko <iovenko@gmail.com>
2023-03-28 20:27:39 -0400

    get rid of warnings

--- review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
+++ review-backend/src/main/java/com/example/review/security/SecurityConfiguration.java
@@ -2,9 +2,9 @@ package com.example.review.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,17 +32,17 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/api/auth");
-        http        
-            .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-            .and().csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
-            .anyRequest().authenticated()
-            .and()
+        http
+            .headers(headers -> headers.frameOptions().disable()).csrf(csrf -> csrf.disable())
+            .authorizeRequests(requests -> requests
+                .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+                .antMatchers(HttpMethod.POST, SecurityConstants.CREATE_REVIEW_PATH).permitAll()
+                .antMatchers(HttpMethod.GET, SecurityConstants.GET_REVIEWS_BY_ISBN_PATH).permitAll()
+                .anyRequest().authenticated())
             .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
             .addFilter(authenticationFilter)
             .addFilterAfter(new JWTAuthorizationFilter(userDetailsService()), AuthenticationFilter.class)
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
